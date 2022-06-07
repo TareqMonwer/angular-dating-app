63ea46abd714a8426904f870aaadd3f3b6006252
TareqMonwer <tareqmonwer137@gmail.com>
2022-06-07 12:40:29 +0600

    :zap: using http and displaying fetched data in UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    /**
+     * DEPRECATED WAY OF USING subsrcibe() method.
+     */
+    // this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(response => {
+    //   this.users = response;
+    // }, error => {
+    //   console.log(error);
+    // })
+
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error)
+    })
+  }
 }
